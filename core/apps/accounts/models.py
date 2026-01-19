dc8a1fd6e57f667b8264d428071f8944189f1336
Hanumanthareddy <reddyreddy884@gmail.com>
2026-01-19 23:05:41 +0530

    Step 3: Implemented Custom User and VendorProfile models

--- core/apps/accounts/models.py
+++ core/apps/accounts/models.py
@@ -1,3 +1,16 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 # Create your models here.
+class User(AbstractUser):
+    is_vendor = models.BooleanField(default=False)
+    is_customer = models.BooleanField(default=True)
+    phone_number = models.CharField(max_length=15, blank=True)
+
+class VendorProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    store_name = models.CharField(max_length=100)
+    is_approved = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.store_name
