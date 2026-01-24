f2597753ba06c3903053c290fd02202eb660a091
Hanumanthareddy <reddyreddy884@gmail.com>
2026-01-24 20:01:44 +0530

    Create first UI like homepage

--- core/apps/store/views.py
+++ core/apps/store/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
-
+from .models import Product
 # Create your views here.
+
+def frontend(request):
+    # Fetch the 8 most recent products
+    products = Product.objects.all()[0:8]
+
+    return render(request,'store/frontend.html',{
+                      'products':products
+                  })
+
