24fb62b207a2ec12c119322bad86766bcbb4ac1b
Hanumanthareddy <reddyreddy884@gmail.com>
2026-02-02 23:26:32 +0530

    Vendor dashboard is creating

--- core/apps/accounts/views.py
+++ core/apps/accounts/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth import login
+from django.contrib.auth.decorators import login_required
 from .forms import SignUpForm
+from store.models import Product
 
 
 # Create your views here.
@@ -13,3 +15,15 @@ def signup(request):
     else:
         form = SignUpForm()
     return render(request, 'accounts/signup.html', {'form': form})
+
+@login_required
+def vendor_dashboard(request):
+    if not request.user.is_vendor:
+        return redirect('frontend')
+
+    # get the product belongs to this vendor
+    products = request.user.products.all()
+
+    # We will eventually add orders here too
+    return render(request, 'accounts/vendor_dashboard.html',
+                  { 'products': products })
